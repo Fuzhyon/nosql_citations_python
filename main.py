4d6288bab16d1a7b939fbfa82843cbdab54c8325
Chris DOMINGUES <chris.domingues@epsi.fr>
2021-03-05 14:48:02 +0100

    feat: connexion à la bdd mongo

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/citations_db"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home_page():
+    online_users = mongo.db.users.find({"online": True})
+    return render_template("index.html",
+        online_users=online_users)
