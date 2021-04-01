94bb66168a4c5f7994ff36f7d9b45f1ff294b6e7
Chris DOMINGUES <chris.domingues@epsi.fr>
2021-04-01 15:34:07 +0200

    refacto: modification du layout du projet

---
+++ citation.py
@@ -0,0 +1,10 @@
+class Citation:
+
+    def __init__(self, texte, auteur=None, oeuvre=None, editeur=None, date=None, langue=None):
+        self._texte = texte
+        self._auteur = auteur
+        self._oeuvre = oeuvre
+        self._editeur = editeur
+        self._date = date
+        self._langue = langue
+
